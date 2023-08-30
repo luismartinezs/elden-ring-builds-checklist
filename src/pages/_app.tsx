202073af13fc58e957587f9ec825055ca185f5f5
Luis Martinez <luismartinezwebdev@gmail.com>
2023-08-30 13:11:16 +0200

    feat: vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,26 @@
+import { type AppType } from "next/app";
+import Head from "next/head";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
+
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
-import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Head>
-        <link rel="icon" href="/elden-ring.png" />
-      </Head>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <SessionProvider session={session}>
+        <Head>
+          <link rel="icon" href="/elden-ring.png" />
+        </Head>
+        <Component {...pageProps} />
+      </SessionProvider>
+      <Analytics />
+    </>
   );
 };
 

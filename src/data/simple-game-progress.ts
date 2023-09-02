c5991f061f1f6f4bd91792a37166627920cfd809
Luis Martinez <luismartinezwebdev@gmail.com>
2023-09-02 23:48:14 +0200

    feat: better description

--- src/data/simple-game-progress.ts
+++ src/data/simple-game-progress.ts
@@ -3,7 +3,7 @@ const list = {
   slug: "simple-game-progress",
   title: "Simple Game Progress",
   notes: [
-    `All non-required bosses marked as optional. Easily reachable ancient smithing stones not marked as optional, others are optional. Weapon upgrades are written as "normal (somber)". Kill any merchants (except for maybe Kale) give their bells to the twin maidens to be able to easily buy smithing stones when you need them. If you're missing stones, explore caves or check Fextralife for locations.\nSimple rule of thumb for leveling: 1) Meet weapon needs first, 2) Every two levels, add one point to VGR and one to your main stat(s), 3) If unsure about your main stat, just boost VGR until you figure it out`
+    `Recommended for playthroughs after the first one (not NG+, starting new save). All non-required bosses marked as optional. Easily reachable ancient smithing stones not marked as optional, others are optional. Weapon upgrades are written as "normal (somber)". Kill any merchants (except for maybe Kale) and give their bells to the twin maidens to be able to easily buy smithing stones when you need them. If you're missing stones, explore caves or check Fextralife for locations.\nSimple rule of thumb for leveling: 1) Meet weapon requirement first, 2) Every two levels, add one point to VGR and one to your main stat(s), 3) If unsure about your main stat, just boost VGR until you figure it out`
   ],
   items: [
     {

43a09ef17a9f13e5d24f0b0308fa421cfece472a
Luis Martinez <luismartinezwebdev@gmail.com>
2023-09-10 19:53:23 +0200

    feat: okay this one

--- src/data/new-game-plus-progress/index.ts
+++ src/data/new-game-plus-progress/index.ts
@@ -3,7 +3,7 @@ import { items } from "~/data/simple-game-progress"
 const list = {
   id: "94321780-5b61-4702-9321-6e363b542d3b",
   slug: "new-game-plus-progress",
-  title: "NG+ Progress",
+  title: "New Game + Progress",
   notes: [],
   items
 }

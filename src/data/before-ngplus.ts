591719379d4d1b5af026e2609c0e3b1fa8924708
Luis Martinez <luismartinezwebdev@gmail.com>
2023-09-10 17:48:43 +0200

    feat: before ngplus

---
+++ src/data/before-ngplus.ts
@@ -0,0 +1,142 @@
+const list = {
+  id: "c155ce72-3691-4d2c-87e6-40636571073d",
+  slug: "before-ngplus",
+  title: "Before NG+",
+  notes: [
+    `Pretty much a checklist for this: <a href="https://eldenring.wiki.fextralife.com/New+Game+Plus" target="_blank">New Game Plus Elden Ring Wiki</a>. This checklist includes everything you might want to do before starting a new NG+ cycle.`,
+  ],
+  items: [
+    {
+      id: '1587abfe-1325-420c-af96-a0af346fcc9a',
+      description: 'Back up your save file (just in case)',
+      tags: ['before-ng+']
+    },
+    {
+      id: '9a2eca56-61f8-40ef-b6d4-26864e3acdf1',
+      description: 'Hand in all Bell Bearings to Twin Maidens',
+      tags: ['before-ng+']
+    },
+    {
+      id: 'dd2079f6-b666-4133-a546-17056850bf57',
+      description: 'Collect as many final Upgrade Materials as possible',
+      tags: ['before-ng+']
+    },
+    {
+      id: 'f75f681e-230a-448d-bc82-91ca64b42a63',
+      description: "Farm Runes to buy out the Merchants' shops",
+      tags: ['before-ng+']
+    },
+    {
+      id: 'dc471971-693c-4fc2-aa46-6d66fb445c8e',
+      description: 'Collect all Cookbooks',
+      tags: ['before-ng+']
+    },
+    {
+      id: '826e3f59-3a5a-4f23-adb4-bfc2d3d4daef',
+      description: 'Collect all Cyrstal Tears',
+      tags: ['before-ng+']
+    },
+    {
+      id: 'f4405ab3-15f2-4b66-b155-fd5f0c5b1a45',
+      description: 'Collect all Whetblades',
+      tags: ['before-ng+']
+    },
+    {
+      id: '031356f5-949b-4006-9f8d-eed051f31a50',
+      description: 'Find and turn in all Scrolls and Prayerbooks to a trainer to purchase their Spells',
+      tags: ['before-ng+']
+    },
+    {
+      id: 'a3677b38-ee7f-4ff0-8450-5ef79c029ad1',
+      description: 'Slay dragons to obtain Dragon Hearts (23) and buy their Incantations at the Cathedral of Dragon Communion (only 20 are required to purchase all Dragon-based Incantations)',
+      tags: ['before-ng+']
+    },
+    {
+      id: 'ab599967-3d87-4f4e-83f6-cce7440b126d',
+      description: 'Obtain all Larval Tears (18) for future Rebirth',
+      tags: ['before-ng+']
+    },
+    {
+      id: '2100cd0e-a75e-4d1e-b575-4dd638ddb634',
+      description: 'Turn in all Deathroots (9) to the Gurranq Beast Clergyman',
+      tags: ['before-ng+']
+    },
+    {
+      id: '3f92aef8-cb98-4172-a7da-9deb82e0b93d',
+      description: 'Kill any NPCs you want the gear from',
+      tags: ['before-ng+']
+    },
+    {
+      id: '9ab33150-2136-46ce-94ac-58b4f5b1ebd7',
+      description: 'Collect Stonesword Keys',
+      tags: ['before-ng+']
+    },
+    {
+      id: '0d09cc46-a61f-4f68-bbdb-da8708a097df',
+      description: 'Trade for any Remembrance Weapons you may wish to dual-wield in the future',
+      tags: ['before-ng+']
+    },
+    {
+      id: '29c3d21d-a469-4c4e-810b-878d0b6b0abe',
+      description: 'Obtain any weapons you will want to dual-wield in the next playthrough',
+      tags: ['before-ng+']
+    },
+    {
+      id: '6cc506dd-61ba-4d7b-93fa-9dd6c24dd35a',
+      description: 'Use Walking Mausoleums (7) to duplicate Remembrances',
+      tags: ['before-ng+']
+    },
+    {
+      id: '0de9a92c-f530-4e2c-881b-c9bf37979ed3',
+      description: 'Collect all non-renewable Crafting Materials',
+      tags: ['before-ng+']
+    },
+    {
+      id: '16578930-d11e-4177-becb-b042f1667151',
+      description: 'Collect all Cracked Pots',
+      tags: ['before-ng+']
+    },
+    {
+      id: '41ac6573-16f8-4d7b-abcb-38f4f972cb47',
+      description: 'Collect all Ritual Pots',
+      tags: ['before-ng+']
+    },
+    {
+      id: '586f610c-e758-4bb5-81f5-9f5535597381',
+      description: 'Collect all Perfume Bottles',
+      tags: ['before-ng+']
+    },
+    {
+      id: '1edee27a-4b70-47ae-a1a6-7e99dd23f3b2',
+      description: 'Collect all Memory Stones',
+      tags: ['before-ng+']
+    },
+    {
+      id: '91a16d94-382b-47ed-a385-690117fb0f8f',
+      description: 'Fully upgrade Sacred Flasks',
+      tags: ['before-ng+']
+    },
+    {
+      id: '3e556999-c60e-408e-ac23-063dbaa03d93',
+      description: 'Collect all talismans',
+      tags: ['before-ng+']
+    },
+    {
+      id: 'c4a514f0-8551-4ff2-8742-f220dae0a579',
+      description: 'Farm any Weapons or Armor that drop from Enemies that you might want (optionally respec for Arcane to increase item discovery)',
+      tags: ['before-ng+']
+    },
+    {
+      id: 'da228138-7029-47d8-a1c2-4bbfd0eec859',
+      description: 'If you plan on playing with a different build on NG+, pick up any equipment for your new build',
+      tags: ['before-ng+']
+    },
+    {
+      id: '1b8d76c8-57f8-4594-8e74-f22f93c17f35',
+      description: 'If you plan on playing with a different build on NG+, respec your character by talking to Rennala',
+      tags: ['before-ng+']
+    }
+  ]
+}
+
+export default list
--- src/data/index.ts
+++ src/data/index.ts
@@ -2,12 +2,15 @@ import templar from './templar-lvl1-50'
 import simpleGameProgress from './simple-game-progress'
 import lightningLancer from './lightning-lancer-50-100'
 import newGamePlusProgress from './new-game-plus-progress'
+import beforeNgPlus from './before-ngplus'
 import { type TChecklist } from '~/features/checklist'
+
 
 const lists: TChecklist[] = [
   // NOTE not sure why but TS gets confused without this type assertion
   simpleGameProgress as TChecklist,
   newGamePlusProgress as TChecklist,
+  beforeNgPlus,
   templar,
   lightningLancer
 ]

04aceb21109f1a4ed74836219f23a591e3ba2d88
Luis Martinez <luismartinezwebdev@gmail.com>
2023-09-10 19:51:33 +0200

    chore: better titles

--- src/data/new-game-plus-progress/index.ts
+++ src/data/new-game-plus-progress/index.ts
@@ -3,7 +3,7 @@ import { items } from "~/data/simple-game-progress"
 const list = {
   id: "94321780-5b61-4702-9321-6e363b542d3b",
   slug: "new-game-plus-progress",
-  title: "New Game Plus Progress",
+  title: "NG+ Progress",
   notes: [],
   items
 }
--- src/data/simple-game-progress/index.ts
+++ src/data/simple-game-progress/index.ts
@@ -3,7 +3,7 @@ import { items } from "./items"
 const list = {
   id: "e8e791ec-4e3a-4a8b-bc32-32a3cb98d694",
   slug: "simple-game-progress",
-  title: "Simple New Game Progress",
+  title: "New Game Progress",
   notes: [
     `Recommended for playthroughs after the first one (not NG+, starting new save). All non-required bosses marked as optional.`, `Easily reachable ancient smithing stones not marked as optional, others are optional. Weapon upgrades are written as "normal (somber)". Kill any merchants (except for maybe Kale) and give their bells to the twin maidens to be able to easily buy smithing stones when you need them. If you're missing stones, explore caves or check Fextralife for locations.`, `Simple rule of thumb for leveling: 1) Meet weapon requirement first, 2) Every two levels, add one point to VGR and one to your main stat(s), 3) If unsure about your main stat, just boost VGR until you figure it out`
   ],

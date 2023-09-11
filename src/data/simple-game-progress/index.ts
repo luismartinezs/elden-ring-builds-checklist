6ee7d8bbe418d064c2589d4ccc04c479eefc1b91
Luis Martinez <luismartinezwebdev@gmail.com>
2023-09-11 21:50:49 +0200

    feat: better notes

--- src/data/simple-game-progress/index.ts
+++ src/data/simple-game-progress/index.ts
@@ -5,7 +5,12 @@ const list = {
   slug: "simple-game-progress",
   title: "New Game Progress",
   notes: [
-    `Recommended for playthroughs after the first one (not NG+, starting new save). All non-required bosses marked as optional.`, `Easily reachable ancient smithing stones not marked as optional, others are optional. Weapon upgrades are written as "normal (somber)". Kill any merchants (except for maybe Kale) and give their bells to the twin maidens to be able to easily buy smithing stones when you need them. If you're missing stones, explore caves or check Fextralife for locations.`, `Simple rule of thumb for leveling: 1) Meet weapon requirement first, 2) Every two levels, add one point to VGR and one to your main stat(s), 3) If unsure about your main stat, just boost VGR until you figure it out`
+    `Recommended for playthroughs after the first one (not NG+, starting new save). All non-required bosses marked as optional.`,
+    `Easily reachable (Somber) Ancient Dragon Smithing Stones not marked as optional, others are optional.`,
+    `Weapon upgrades are written as "normal (somber)".`,
+    `Kill any merchants (except for maybe Kale) and give their bells to the twin maidens to be able to easily buy smithing stones when you need them.`,
+    `If you're missing stones to upgrade your weapons you can: A) explore caves, B) check Fextralife for locations, or C) get the Bell Bearings that allow to buy upgrade materials from Twin Maidens.`,
+    `Simple rule of thumb for leveling: 1) Meet weapon requirement first, 2) Every two levels, add one point to VGR and one to your main stat(s), 3) If unsure about your main stat, just boost VGR until you figure it out`
   ],
   items
 }

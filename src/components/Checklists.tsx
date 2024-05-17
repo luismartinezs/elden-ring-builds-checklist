a0ea41258d078cca0fa4ac3de49569f5a1347324
Luis Martinez <luismartinezwebdev@gmail.com>
2024-05-17 18:04:41 +0700

    tweak tags

--- src/components/Checklists.tsx
+++ src/components/Checklists.tsx
@@ -29,33 +29,28 @@ const tagMap: Record<
   },
   mid: {
     label: "Mid",
-    className:
-      "bg-gray-800 text-white border border-gray-500",
+    className: "bg-gray-800 text-white border border-gray-500",
   },
   early: {
     label: "Early",
-    className:
-      "bg-gray-800 text-white border border-purple-500",
+    className: "bg-gray-800 text-white border border-purple-500",
   },
   late: {
     label: "Late",
-    className:
-      "bg-gray-800 text-white border border-indigo-500",
+    className: "bg-gray-800 text-white border border-indigo-500",
   },
   ngplus: {
     label: "NG+",
-    className:
-      "bg-gray-800 text-white border border-pink-500",
+    className: "bg-gray-800 text-white border border-pink-500",
   },
   new: {
-    label: 'New',
+    label: "New",
     className:
-      'bg-gradient-to-r from-amber-300 to-yellow-300 text-amber-900 border border-amber-700 outline outline-offset-2 outline-2 outline-yellow-300',
+      "bg-gradient-to-r from-amber-300 to-yellow-300 text-amber-900 border border-amber-700 outline outline-offset-2 outline-2 outline-yellow-300",
   },
   default: {
     label: "tag",
-    className:
-      "bg-gray-200 text-gray-800 border border-gray-400",
+    className: "bg-gray-200 text-gray-800 border border-gray-400",
   },
 } as const;
 
@@ -90,18 +85,24 @@ export function Checklists({
       <Heading.H2 id="checklists">Checklists</Heading.H2>
       <ul className="mt-4 flex flex-col gap-2 text-2xl">
         {Object.values(checklists).map(({ slug, title, tags }) => (
-          <li key={slug} className="flex items-center gap-1">
-            <Link href={`/checklists/${slug}`} className="">{title}</Link>
+          <li key={slug} className="flex flex-wrap items-start gap-1 border-t border-stone-600 first:border-0 pt-1 text-base md:text-lg lg:text-2xl">
+            <Link href={`/checklists/${slug}`} className="flex-shrink-0">
+              {title}
+            </Link>
             {tags && (
-              <ul className="ml-1 flex flex-row items-center justify-center gap-2 relative bottom-1">
+              <ul className="ml-1 lg:ml-2 flex flex-wrap items-center gap-2 relative lg:bottom-0.5">
                 {tags.map((tag) => {
-                  const {label, className} = getTag(tag);
+                  const { label, className } = getTag(tag);
 
                   return (
-                  <li key={tag} className="">
-                    <DumbTag label={label} className={cn(className, 'rounded-md')} />
-                  </li>
-                )})}
+                    <li key={tag} className="flex-shrink-0">
+                      <DumbTag
+                        label={label}
+                        className={cn(className, "rounded-md lg:text-base")}
+                      />
+                    </li>
+                  );
+                })}
               </ul>
             )}
           </li>

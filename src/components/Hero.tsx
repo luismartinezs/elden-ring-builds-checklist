335ab90ab64c7de657ff90a4e4365c331ca1c5d7
Luis Martinez <luismartinezwebdev@gmail.com>
2024-05-18 17:32:19 +0700

    fix build

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Image from "next/image";
-import Link from "next/link";
 import { Heading } from "./Heading";
 import { SectionWrapper } from "./SectionWrapper";
 

27af142deed7c51548ec099b66d13b51459cc460
Luis Martinez <luismartinezwebdev@gmail.com>
2024-06-25 12:11:13 +0700

    cookies modal

---
+++ src/components/CookieModal.tsx
@@ -0,0 +1,122 @@
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { cn } from "~/utils/cn";
+
+const defaultMessage = (
+  <p>
+    We use cookies to provide our services and for analytics and marketing. To
+    find out more about our use of cookies, please see our{" "}
+    <Link
+      href="/privacy-policy"
+      className="text-primary-100 underline hover:text-primary-200"
+    >
+      Privacy Policy
+    </Link>
+    . By continuing to browse our website, you agree to our use of cookies.
+  </p>
+);
+
+const Separator = ({ thickness = 1 }) => {
+  return (
+    <div
+      className={cn(
+        "elden-ring-fade-separator w-full",
+        thickness === 1 ? "h-px" : `h-[${thickness}px]`
+      )}
+    ></div>
+  );
+};
+
+const CookieModal = ({
+  message = defaultMessage,
+  acceptLabel = "Accept",
+  denyLabel = "Deny",
+}) => {
+  const [open, setOpen] = useState(true);
+
+  const getGdpr = () => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("GDPR:accepted");
+    }
+    return null;
+  };
+
+  useEffect(() => {
+    if (!getGdpr()) {
+      setOpen(true);
+    } else {
+      setOpen(false);
+    }
+  }, []);
+
+  const accept = () => {
+    if (typeof window !== "undefined") {
+      setOpen(false);
+      localStorage.setItem("GDPR:accepted", "true");
+    }
+  };
+
+  // const deny = () => {
+  //   if (typeof window !== "undefined") {
+  //     setOpen(false);
+  //     localStorage.setItem("GDPR:accepted", "false");
+  //   }
+  // };
+
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      if (event.key === "E" || event.key === "e") {
+        accept();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [accept]);
+
+  if (!open) {
+    return null;
+  }
+
+  return (
+    <div
+      className="fixed bottom-10 z-50 w-full elden-ring-background text-white"
+    >
+      <Separator thickness={2} />
+      <div className="relative py-2">
+        <div className="mx-auto mb-4 flex max-w-xl flex-col items-center justify-center space-y-2 px-4 md:px-0 lg:flex-row lg:space-x-2 lg:space-y-0">
+          <div className="flex items-center justify-center space-x-2">
+            <div className="flex-grow text-center font-serif">
+              <div>{message}</div>
+            </div>
+          </div>
+        </div>
+        <Separator />
+        <button
+          className="group flex flex-shrink items-baseline justify-center pt-1 w-full"
+          onClick={accept}
+        >
+          <div
+            className="mr-1 flex size-8 items-center justify-center rounded-sm border-2 border-stone-600 px-2 text-2xl text-white group-hover:bg-stone-700"
+            onClick={accept}
+          >
+            <span>E</span>
+          </div>
+          <span className="font-serif">: {acceptLabel}</span>
+          <div
+            className="absolute inset-x-0 bottom-0 h-1"
+            style={{
+              background:
+                "linear-gradient(to right, transparent, rgba(68, 64, 60, 0.1) 10%, rgba(68, 64, 60, 0.1) 90%, transparent)",
+            }}
+          />
+        </button>
+      </div>
+      <Separator thickness={2} />
+    </div>
+  );
+};
+
+export default CookieModal;
--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -3,7 +3,8 @@ import { Feedback } from "~/features/feedback";
 import { Footer } from "~/components/Footer";
 import { Header } from "~/components/Header";
 import { ShareModal, useShareModal } from "~/features/share";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import CookieModal from "~/components/CookieModal";
 
 export function PageLayout({ children }: { children: React.ReactNode }) {
   const { showModal, handleClose, handleShare } = useShareModal();
@@ -12,9 +13,7 @@ export function PageLayout({ children }: { children: React.ReactNode }) {
     <div className="relative isolate flex h-full min-h-screen flex-col justify-between">
       <div>
         <Header className="sticky top-0 z-30" />
-        <main className="relative isolate">
-          {children}
-        </main>
+        <main className="relative isolate">{children}</main>
       </div>
       <ShareModal
         isOpen={showModal}
@@ -22,6 +21,14 @@ export function PageLayout({ children }: { children: React.ReactNode }) {
         onShare={() => handleShare()}
       />
       <Footer />
+      <ClientOnly>
+      <CookieModal
+          message={<p>
+            Ah, tarnished one. Know this: thy visits are observed through analytics most compliant with the edicts of GDPR. By granting thy consent, thou dost acknowledge this truth.
+          </p>}
+          acceptLabel="Embrace Edict"
+        />
+      </ClientOnly>
       <ClientOnly>
         <Feedback />
       </ClientOnly>

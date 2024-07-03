d1fe59a4d05a14845b002f0fd0a180d81964ada2
Luis Martinez <luismartinezwebdev@gmail.com>
2024-07-03 12:24:19 +0700

    fix sitemap typo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://eldenringbuilds.vercel.app/',
   generateRobotsTxt: true,
-  changefreq: 'weely',
-  // ...other options
+  changefreq: 'weekly',
 }

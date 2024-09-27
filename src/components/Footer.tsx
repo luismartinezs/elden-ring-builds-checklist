436a9e38a628a6da0f413136ba2858f792d91762
Luis Martinez <luismartinezwebdev@gmail.com>
2024-09-27 18:44:15 +0700

    add extra links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 import { AiFillGithub } from "react-icons/ai";
 import { FaXTwitter } from "react-icons/fa6";
+import Link from "next/link";
 
-const links = [
+const socialLinks = [
   {
     label: "X",
     icon: FaXTwitter,
@@ -14,13 +15,36 @@ const links = [
   },
 ];
 
+const pageLinks = [
+  { label: "Contact", href: "/contact" },
+  { label: "Privacy Policy", href: "/privacy-policy" },
+];
+
 export function Footer() {
   return (
     <footer className="container mx-auto max-w-7xl px-2 py-2 font-medium xl:px-0">
-      <div className="flex justify-between">
-        <div className="text-sm text-stone-400"><a href="https://www.webdevluis.com/" target="_blank">Luis Martinez</a> @2024</div>
+      <div className="flex flex-col space-y-4 sm:flex-row sm:justify-between sm:space-y-0">
+        <div className="flex flex-col space-y-2 sm:space-y-0 sm:flex-row space-x-6 text-sm text-stone-400 items-center">
+          <div className="flex items-center space-x-1">
+            <a
+              href="https://www.webdevluis.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Luis Martinez
+            </a>
+            <span>@2024</span>
+          </div>
+          <div className="flex space-x-4">
+            {pageLinks.map(({ label, href }) => (
+              <Link key={label} href={href} className="hover:text-stone-500">
+                {label}
+              </Link>
+            ))}
+          </div>
+        </div>
         <div className="flex justify-center space-x-6">
-          {links.map(({ label, icon: Icon, href }) => (
+          {socialLinks.map(({ label, icon: Icon, href }) => (
             <a
               key={label}
               href={href}

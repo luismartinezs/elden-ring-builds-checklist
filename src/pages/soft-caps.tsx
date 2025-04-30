5727063feaabad9cdb1ee5fee9e78887e86c1790
Luis Martinez <luismartinezwebdev@gmail.com>
2025-04-30 13:24:22 +0700

    seo

--- src/pages/soft-caps.tsx
+++ src/pages/soft-caps.tsx
@@ -2,7 +2,6 @@ import Head from "next/head";
 import React from "react";
 import { Heading } from "~/components/Heading";
 import { SectionWrapper } from "~/components/SectionWrapper";
-import { levelCapsData } from "~/data/level-caps-data";
 import { PageLayout } from "~/layouts/PageLayout";
 import {stats} from "~/features/stats/stats";
 import { StatSoftCapItem } from "~/features/stats/StatSoftCapItem";
@@ -12,22 +11,22 @@ const LevelCapsPage = () => {
   return (
     <PageLayout>
       <Head>
-        <title>Level Caps | Elden Ring Checklists - Elden Hub</title>
+        <title>Soft Caps | Elden Ring Checklists - Elden Hub</title>
         <meta
           name="description"
           content="Soft caps and recommended investment levels for Vigor, Mind, Endurance, Strength, Dexterity, Intelligence, Faith, and Arcane in Elden Ring."
         />
         <link
           rel="canonical"
-          href="https://eldenhub.appforgelabs.com/level-caps"
+          href="https://eldenhub.appforgelabs.com/soft-caps"
           key="canonical"
         />
       </Head>
       <SectionWrapper>
-        <Heading.H1>Stat Level Caps</Heading.H1>
+        <Heading.H1>Stat Soft Caps</Heading.H1>
 
         <Paragraph>
-          Stat soft caps can get complicated. If you see a mistake, let me know via the feedback button
+          Soft caps can get complicated. If you see a mistake, let me know via the feedback button
         </Paragraph>
 
         <Paragraph>All stats hard cap at 99</Paragraph>
